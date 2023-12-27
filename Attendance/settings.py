97b5dc5cddadfca0ec5dcd40894404843ac80c77
Smithis <88709267+Smithis@users.noreply.github.com>
2023-12-27 09:02:14 -0500

    Update settings.py

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i6170babfa0ei4o0#s-m-zlw_orgx(k$+#cmsb_z-pt$j1%6%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
