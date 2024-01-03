0e733b0e00588cd349397f1a8251d711b53c744c
Smithis <88709267+Smithis@users.noreply.github.com>
2024-01-03 06:54:21 -0500

    Update settings.py

--- Attendance/settings.py
+++ Attendance/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'studentattendance.apps.StudentattendanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
