676657db08f159a582c235e0e917e7056865e16e
Smithis <88709267+Smithis@users.noreply.github.com>
2024-02-02 11:23:32 -0500

    Update urls.py

--- studentattendance/urls.py
+++ studentattendance/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home,main,apid,AdsVIew
+from .views import home,main,apid
 
 urlpatterns = [
     path('',home),
     path('main',main),
-    path('api/<str:roll>',apid),
-    path('ads.txt',AdsVIew)
+    path('api/<str:roll>',apid)
 ]
